3a300edc12a10a889279d71bc85bc147bb26515e
Lakshmi Kavya <vankadaralakshmikavya@gmail.com>
2022-02-24 13:01:15 +0530

    Card Work

--- src/components/HomePage/HomeCards.js
+++ src/components/HomePage/HomeCards.js
@@ -1,41 +1,6 @@
 import styled from "styled-components"
 
 const DIV = styled.div`
-     /* border:1px solid tomato;
-    border-radius: 10px;
-    position: relative;
-    height: 400px;
-    background-image:url(${({image})=>image});
-     width:100%;
-     -webkit-background-size: cover;
-     background-size: cover;
-     background-position:center center;
-     border-radius: 10px;
-     z-index : 10; */
-
-     /* &:after{
-    position:absolute;
-    background: inherit;
-    z-index: -1;
-    bottom: 0;
-    content: "";
-    width: 100%;
-    height:100%; */
- /* } */
-
- /* #Profile-Details{
-    background: #fff;
-    z-index: 8;
-    color: black;
-    width: 100%;
-
-} */
-
-/* #Profile-Details:after{
-   transform-origin : right bottom;
-   transform : skewY(-3deg)
-} */
-
 *{
  padding:0;
  margin: 0; 
@@ -46,9 +11,9 @@ const DIV = styled.div`
     height:100vh;
     background-color: rgb(34, 117, 189);
     color: black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    /* display: flex; */
+    /* justify-content: center;
+    align-items: center; */
 }
 .Home-Card{
     flex-basis: 20%;
@@ -59,18 +24,62 @@ const DIV = styled.div`
 .card-title{
     background-image:url(${({image})=>image});
     width:100%;
-    height:300px;
+    height:250px;
     background-size: cover;
-   -webkit-clip-path: polygon(0 0, 100% 0, 100% 60%, 0 85%);
-   clip-path: polygon(0 0, 100% 0, 100% 60%, 0 85%);
-   border-radius: 10px;
-  
+   -webkit-clip-path: polygon(0 0, 100% 0, 100% 80%, 0 100%);
+   clip-path: polygon(0 0, 100% 0, 100% 80%, 0 100%);
+   border-radius: 10px; 
 }
+
+/* .Home-Card:hover .card-title, .Home-Card:focus .card-title{
+  transform: scale(1.2);
+} */
+
 .card-body{
-    padding: 15px;;
+    padding: 6.5%;
 }
-h2{
-    margin: 10px;
+
+.Inner-Home-Card{
+   width:100%;
+   height: 50px;
+   display: grid ;
+   grid-template-columns: 75% 20%;
+   justify-content:space-between;
+   text-align: left;
+   margin: auto;
+   margin-top : 8%;
+}
+
+.Home-Profile{
+   display: flex ;
+   gap: 6%;
+}
+
+.Home-Profile-Dp{
+   height:90%;
+   width:22.5%;
+   border-radius: 50%;
+}
+
+.Home-Profile-Dp > img{
+   height:100%;
+   width:100%;
+   border-radius: 50%;
+}
+
+.Home-Profile-Details{
+   padding-top: 3%;
+   line-height: 20px;
+}
+
+.Home-Video-Count{
+   padding-top: 10%;
+   text-align: center;
+   line-height: 20px;
+}
+
+.card-body h3{
+   text-align:left;
 }
 
 `
@@ -99,8 +108,30 @@ export const HomeCards = ({items})=>{
            <div className="Home-Card">
               <div className="card-title"> </div> 
               <div className="card-body">
-                <h2>title of the card</h2>
-                 <div></div>
+                <h3>Entrepreneurship 101</h3>
+                 <div className="Inner-Home-Card">
+                     <div className="Home-Profile">
+                        <div className="Home-Profile-Dp">
+                           <img src={items}/>
+                        </div>
+                        <div className="Home-Profile-Details">
+                            <h4>RITESH AGARWAL</h4>
+                            <p style={{ 
+                               fontSize:"11px",
+                               fontWeight:"500",
+                               color:"#999999"
+                            }}>FOUNDER, OYO ROOMS</p>
+                        </div>
+                     </div>
+                     <div className="Home-Video-Count"> 
+                     <h4>9</h4>
+                     <p style={{ 
+                         fontSize:"11px",
+                         fontWeight:"500",
+                         color:"#999999"
+                      }}>VIDEOS</p>
+                     </div>
+                 </div>
             </div>
           </div>
       </DIV>
