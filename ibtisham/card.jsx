de3129d5d5421fad62d12a6176eedf779dbfb1d5
ibtishamibtisham <shaguftamansuri987@gmail.com>
2022-02-24 11:22:28 +0530

    cards

---
+++ ibtisham/card.jsx
@@ -0,0 +1,20 @@
+import "./card.css"
+export const Cards=()=>{
+return(<>
+
+<div className="container">
+    <div className="card">
+        <div className="card-title">
+           
+   
+    </div> <div className="card-body">
+                <h2>title of the card</h2>
+                <p>body of the card</p>
+            </div>
+   
+    </div>
+   
+    </div>
+ 
+</>)
+}
