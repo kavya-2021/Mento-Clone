56cb9534c618ba51bdf908f082963881106d0070
Babita Bisht <84259086+Bbisht01@users.noreply.github.com>
2022-02-26 20:31:00 +0530

    Update Alltopics.jsx

--- src/components/AllTopicFolder/Alltopics.jsx
+++ src/components/AllTopicFolder/Alltopics.jsx
@@ -35,7 +35,7 @@ function Alltopics() {
 
   return (
    <div style={{backgroundImage: "url('https://www.mento.co.in/assets/all-topics-bg-c70ac523da68e4c48726cc16261ddb6d5b08a7f43fa4dc0e78f5488466e78514.png')",   backgroundColor: "#1a1d24"}}>
-     <h2 style={{width:"20%",color:"#f36f21",fontWeight:"500",position:'absolute',marginTop:"9%",marginLeft:"29%",textTransform:"uppercase"}}>{changedTopic}</h2>
+     <h2 style={{width:"20%",color:"#f36f21",fontWeight:"500",position:'absolute',marginTop:"9%",marginLeft:"24%",textTransform:"uppercase"}}>{changedTopic}</h2>
       <div className="MainContainer">
       
       <SidebarContent>
