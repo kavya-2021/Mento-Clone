071eb2b04a4d094ff8e29c625a737f72f1e7715a
Lakshmi Kavya <vankadaralakshmikavya@gmail.com>
2022-02-27 14:23:58 +0530

    NavBar - Made my Surya

--- src/components/NavBar/About.js
+++ src/components/NavBar/About.js
@@ -1,5 +1,41 @@
+import "./about.css"
+
 export const About = ()=>{
     return (
-        <div> Hey I'm About Page </div>
+      
+        <div id="abotmain">
+        <h1 >
+        Ever wondered what you really like doing ? What is your true calling?
+        </h1>
+        <p>
+        We often end up in careers or industries because we are either confused or clueless about what we want to do in life. Also, we don't actually have much information about the numerous career options available to us.
+
+        </p>
+        <p>
+        Mento aims to solve that problem by providing first hand in-depth career advice from the biggest and best in each field. You'll get all the information you need to decide if a particular career is for you or not.
+        </p>
+   <div id="video">
+   <iframe width="500" height="315" src="https://www.youtube.com/embed/0BlEW1nEF3s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+   </div>
+   <p>From the biggest entrepreneur in India - Ritesh Agarwal, the director of the highest grossing film ever made in India - Nitesh Tiwari, to the biggest selling author in India - Amish Tripathi, the founders of the number one restaurant in India - Bombay Canteen, to the creator of three of the biggest web-series shows in India - Biswapati Sarkar amongst numerous others, Mento brings you an all star lineup to help you decide your future.</p>
+
+<p>
+Over the coming year we will have a rich content library of over 300 different career options to choose from. You'll also get all the information on how to do a certified course for a particular field as well as find internship opportunities to learn on the job.
+We look forward to helping millions of Indians find their calling and discover their dream career.
+Cheers to a new India!
+</p>
+
+<p id="center">
+P.S : Please send me your thoughts on
+</p>
+
+  <div id="center2">
+      
+      SUPPORT@MENTO.CO.IN
+  </div>
+        </div>
+
+        
     )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,55 @@
+import { Link} from "react-router-dom"
+import "./navbar.css"
+
+export const Navbar = () => {
+
+//     const navigate = useNavigate();
+// // const  gotoabot = ()=>{
+// //     navigate('./about')
+// // }
+
+   
+    return (
+       
+        <div id="fix">
+        <div id="main_navbar">
+
+        <Link to={'/'}> 
+            <div id="logo">
+                <img src="./favicon.ico" alt="" />
+            </div>  
+        </Link>
+
+     
+            <div id="options">
+                <div id="input_search" >
+                    
+                    <input className="input-elevated" type="text" placeholder=" Topic name,Infulencers name
+" />
+                </div>
+
+                <Link to={'/alltopics'} style={{
+                    textDecoration:"none",
+                    color:"white"
+                }}> <div id="alltopic">ALL TOPICS</div>  </Link>
+
+                <Link to={'/about'} style={{
+                    textDecoration:"none",
+                    color:"white"
+                }}> <div id="about">ABOUT</div>  </Link>
+                
+                <button className="btn-hover color-2">LOGIN</button>
+               
+               
+            </div>
+       
+        </div>
+        </div>
+       
+    )
+    
+    
+    }
+
+
+    
